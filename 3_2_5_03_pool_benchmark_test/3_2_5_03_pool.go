80866c2e34408f41bcc650b6004374594000c6aa
suganoo <suganoo2002@gmail.com>
2019-04-03 19:03:21 +0900

    add file

---
+++ 3_2_5_03_pool_benchmark_test/3_2_5_03_pool.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+	"net"
+)
+
+func connectToService() interface{} {
+	time.Sleep(1 * time.Second)
+	return struct{}{}
+}
+
+func startNetworkDaemon() *sync.WaitGroup {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		server, err := net.Listen("tcp", "localhost:8081")
+		if err != nil {
+			log.Fatalf("cannot listen: %v", err)
+		}
+		defer server.Close()
+
+		wg.Done()
+
+		for {
+			conn, err := server.Accept()
+			if err != nil {
+				log.Printf("cannot accept connection: %v", err)
+				continue
+			}
+			connectToService()
+			fmt.Fprintln(conn, "")
+			conn.Close()
+		}
+	}()
+	return &wg
+}
---
+++ 3_3_02_channel.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	stringStream := make(chan string)
+	go func() {
+		stringStream <- "Hello channels!"
+	}()
+	
+	salutation, ok := <- stringStream
+	fmt.Printf("(%v): %v", ok, salutation)
+}
