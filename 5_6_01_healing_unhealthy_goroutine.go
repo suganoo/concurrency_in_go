1b9c720a57884d35df1eabda9b18f5d69bfdba96
suganuma <suganoo2002@gmail.com>
2019-04-13 00:35:52 +0900

    add file

--- 5_6_01_healing_unhealthy_goroutine.go
+++ 5_6_01_healing_unhealthy_goroutine.go
@@ -56,7 +56,7 @@ func main() {
 
 				var warDone chan interface{}
 				var wardHeartbeat <-chan interface{}
-				startWard := func() {{
+				startWard := func() {
 					warDone = make(chan interface{})
 					wardHeartbeat = startGoroutine(or(warDone, done), timeout/2)
 				}
@@ -91,4 +91,26 @@ func main() {
 			return heartbeat
 		}
 	}
+	log.SetOutput(os.Stdout)
+	log.SetFlags(log.Ltime | log.LUTC)
+
+	doWork := func(done <-chan interface{}, _ time.Duration) <-chan interface{} {
+		log.Println("ward: Hello, I'm irresponsible!")
+		go func() {
+			<-done
+			log.Println("ward: I am halting.")
+		}()
+		return nil
+	}
+	doWorkWithSteward := newSteward(4*time.Second, doWork)
+
+	done := make(chan interface{})
+	time.AfterFunc(9*time.Second, func() {
+		log.Println("main: halting steward and ward.")
+		close(done)
+	})
+
+	for range doWorkWithSteward(done, 4*time.Second) {
+	}
+	log.Println("Done")
 }
---
+++ 5_6_02_healing_unhealthy_goroutine.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+func main() {
+	var or func(channels ...<-chan interface{}) <-chan interface{}
+	or = func(channels ...<-chan interface{}) <-chan interface{} {
+		switch len(channels) {
+		case 0:
+			return nil
+		case 1:
+			return channels[0]
+		}
+
+		orDone := make(chan interface{})
+		go func() {
+			defer close(orDone)
+
+			switch len(channels) {
+			case 2:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				}
+			default:
+				select {
+				case <-channels[0]:
+				case <-channels[1]:
+				case <-channels[2]:
+				case <-or(append(channels[3:], orDone)...):
+				}
+			}
+		}()
+		return orDone
+	}
+	type startGoroutineFn func(
+		done <-chan interface{},
+		pulseInterval time.Duration,
+	) (heartbeat <-chan interface{})
+
+	newSteward := func(
+		timeout time.Duration,
+		startGoroutine startGoroutineFn,
+	) startGoroutineFn {
+		return func(
+			done <-chan interface{},
+			pulseInterval time.Duration,
+		) <-chan interface{} {
+			heartbeat := make(chan interface{})
+			go func() {
+				defer close(heartbeat)
+
+				var warDone chan interface{}
+				var wardHeartbeat <-chan interface{}
+				startWard := func() {
+					warDone = make(chan interface{})
+					wardHeartbeat = startGoroutine(or(warDone, done), timeout/2)
+				}
+				startWard()
+				pulse := time.Tick(pulseInterval)
+
+			monitorLoop:
+				for {
+					timeoutSignal := time.After(timeout)
+
+					for {
+						select {
+						case <-pulse:
+							select {
+							case heartbeat <- struct{}{}:
+							default:
+							}
+						case <-wardHeartbeat:
+							continue monitorLoop
+						case <-timeoutSignal:
+							log.Println("steward: ward unhealthy; restarting")
+							close(warDone)
+							startWard()
+							continue monitorLoop
+						case <-done:
+							return
+						}
+					}
+				}
+			}()
+
+			return heartbeat
+		}
+	}
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+	bridge := func(
+	    done <-chan interface{},
+	    chanStream <-chan <-chan interface{},
+	) <-chan interface{} {
+	    valStream := make(chan interface{}) // <1>
+	    go func() {
+	        defer close(valStream)
+	        for { // <2>
+	            var stream <-chan interface{}
+	            select {
+	            case maybeStream, ok := <-chanStream:
+	                if ok == false {
+	                    return
+	                }
+	                stream = maybeStream
+	            case <-done:
+	                return
+	            }
+	            //for val := range orDone(done, stream) { // <3>
+	            for val := range or(done, stream) { // <3>
+	                select {
+	                case valStream <- val:
+	                case <-done:
+	                }
+	            }
+	        }
+	    }()
+	    return valStream
+	}
+	doWorkFn := func(
+		done <-chan interface{},
+		intList ...int,
+	) (startGoroutineFn, <-chan interface{}) {
+		intChanStream := make(chan (<-chan interface{}))
+		intStream := bridge(done, intChanStream)
+		doWork := func(
+			done <-chan interface{},
+			pulseInterval time.Duration,
+		) <-chan interface{} {
+			intStream := make(chan interface{})
+			heartbeat := make(chan interface{})
+			go func() {
+				defer close(intStream)
+				select {
+				case intChanStream <- intStream:
+				case <-done:
+					return
+				}
+
+				pulse := time.Tick(pulseInterval)
+
+				for {
+				valueLoop:
+					for _, intVal := range intList {
+						if intVal < 0 {
+							log.Printf("negative value: %v\n", intVal)
+							return
+						}
+
+						for {
+							select {
+							case <-pulse:
+								select {
+								case heartbeat <- struct{}{}:
+								default:
+								}
+							case intStream <- intVal:
+								continue valueLoop
+							case <-done:
+								return
+							}
+						}
+					}
+				}
+			}()
+			return heartbeat
+		}
+		return doWork, intStream
+	}
+	log.SetFlags(log.Ltime | log.LUTC)
+	log.SetOutput(os.Stdout)
+
+	done := make(chan interface{})
+	defer close(done)
+
+	doWork, intStream := doWorkFn(done, 1, 2, -1, 3, 4, 5)
+	doWorkWithSteward := newSteward(1*time.Second, doWork)
+	doWorkWithSteward(done, 1*time.Hour)
+
+	for intVal := range take(done, intStream, 6) {
+		fmt.Printf("Received: %v\n", intVal)
+	}
+}
